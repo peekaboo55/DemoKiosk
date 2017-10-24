df746b53fcd6cd9137ebbb3a678d144165453909
Peerawat <peejenpq@gmail.com>
2017-10-24 16:25:07 +0700

    Add files via upload

---
+++ DemoQue/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace DemoQue
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.bt_pic1 = new System.Windows.Forms.PictureBox();
+            this.bt_pic2 = new System.Windows.Forms.PictureBox();
+            this.bt_pic3 = new System.Windows.Forms.PictureBox();
+            this.bt_pic4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.tableLayoutPanel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bt_pic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bt_pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bt_pic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bt_pic4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tableLayoutPanel1.BackgroundImage")));
+            this.tableLayoutPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1008, 729);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel4, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 75);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1002, 577);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.axWindowsMediaPlayer1, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.textBox1, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 28.37128F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 71.62872F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(495, 571);
+            this.tableLayoutPanel3.TabIndex = 0;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(3, 165);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(489, 403);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("Bernard MT Condensed", 90F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Red;
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(489, 150);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.ColumnCount = 1;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel4.Controls.Add(this.bt_pic1, 0, 1);
+            this.tableLayoutPanel4.Controls.Add(this.bt_pic2, 0, 3);
+            this.tableLayoutPanel4.Controls.Add(this.bt_pic3, 0, 5);
+            this.tableLayoutPanel4.Controls.Add(this.bt_pic4, 0, 7);
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(504, 3);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 9;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(495, 571);
+            this.tableLayoutPanel4.TabIndex = 1;
+            // 
+            // bt_pic1
+            // 
+            this.bt_pic1.BackColor = System.Drawing.Color.Transparent;
+            this.bt_pic1.BackgroundImage = global::DemoQue.Properties.Resources.A;
+            this.bt_pic1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bt_pic1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bt_pic1.Location = new System.Drawing.Point(3, 25);
+            this.bt_pic1.Name = "bt_pic1";
+            this.bt_pic1.Size = new System.Drawing.Size(489, 108);
+            this.bt_pic1.TabIndex = 0;
+            this.bt_pic1.TabStop = false;
+            this.bt_pic1.Click += new System.EventHandler(this.bt_pic1_Click);
+            // 
+            // bt_pic2
+            // 
+            this.bt_pic2.BackColor = System.Drawing.Color.Transparent;
+            this.bt_pic2.BackgroundImage = global::DemoQue.Properties.Resources.B;
+            this.bt_pic2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bt_pic2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bt_pic2.Location = new System.Drawing.Point(3, 161);
+            this.bt_pic2.Name = "bt_pic2";
+            this.bt_pic2.Size = new System.Drawing.Size(489, 108);
+            this.bt_pic2.TabIndex = 1;
+            this.bt_pic2.TabStop = false;
+            this.bt_pic2.Click += new System.EventHandler(this.bt_pic2_Click);
+            // 
+            // bt_pic3
+            // 
+            this.bt_pic3.BackColor = System.Drawing.Color.Transparent;
+            this.bt_pic3.BackgroundImage = global::DemoQue.Properties.Resources.C;
+            this.bt_pic3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bt_pic3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bt_pic3.Location = new System.Drawing.Point(3, 297);
+            this.bt_pic3.Name = "bt_pic3";
+            this.bt_pic3.Size = new System.Drawing.Size(489, 108);
+            this.bt_pic3.TabIndex = 2;
+            this.bt_pic3.TabStop = false;
+            this.bt_pic3.Click += new System.EventHandler(this.bt_pic3_Click);
+            // 
+            // bt_pic4
+            // 
+            this.bt_pic4.BackColor = System.Drawing.Color.Transparent;
+            this.bt_pic4.BackgroundImage = global::DemoQue.Properties.Resources.D;
+            this.bt_pic4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bt_pic4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bt_pic4.Location = new System.Drawing.Point(3, 433);
+            this.bt_pic4.Name = "bt_pic4";
+            this.bt_pic4.Size = new System.Drawing.Size(489, 108);
+            this.bt_pic4.TabIndex = 3;
+            this.bt_pic4.TabStop = false;
+            this.bt_pic4.Click += new System.EventHandler(this.bt_pic4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1002, 66);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = global::DemoQue.Properties.Resources.S_6853921553409;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(3, 658);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1002, 68);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Smart Queue";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.tableLayoutPanel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.bt_pic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bt_pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bt_pic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bt_pic4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.PictureBox bt_pic1;
+        private System.Windows.Forms.PictureBox bt_pic2;
+        private System.Windows.Forms.PictureBox bt_pic3;
+        private System.Windows.Forms.PictureBox bt_pic4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+    }
+}
+
---
+++ DemoQue/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Zen;
+
+namespace DemoQue
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Zen.Barcode.CodeQrBarcodeDraw qrcode = Zen.Barcode.BarcodeDrawFactory.CodeQr;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.settings.setMode("loop", true);
+            FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+        }
+
+        private void bt_pic1_Click(object sender, EventArgs e)
+        {
+            PrintDocument pd = new PrintDocument();
+            pd.DocumentName = "Test Document";
+            pd.PrintPage += new PrintPageEventHandler(printDocument1_PrintPage);
+            pd.Print();
+            MessageBox.Show("ทำรายการเสร็จสิ้น");
+        }
+
+        private void bt_pic2_Click(object sender, EventArgs e)
+        {
+            PrintDocument pd = new PrintDocument();
+            pd.DocumentName = "Test Document";
+            pd.PrintPage += new PrintPageEventHandler(printDocument1_PrintPage);
+            pd.Print();
+            MessageBox.Show("ทำรายการเสร็จสิ้น");
+        }
+
+        private void bt_pic3_Click(object sender, EventArgs e)
+        {
+            PrintDocument pd = new PrintDocument();
+            pd.DocumentName = "Test Document";
+            pd.PrintPage += new PrintPageEventHandler(printDocument1_PrintPage);
+            pd.Print();
+            MessageBox.Show("ทำรายการเสร็จสิ้น");
+        }
+
+        private void bt_pic4_Click(object sender, EventArgs e)
+        {
+            PrintDocument pd = new PrintDocument();
+            pd.DocumentName = "Test Document";
+            pd.PrintPage += new PrintPageEventHandler(printDocument1_PrintPage);
+            pd.Print();
+            MessageBox.Show("ทำรายการเสร็จสิ้น");
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            float x, y, lineOffset, center;
+
+            // Set Font
+            Font printFont = new Font("Superspace Bold", (float)10, FontStyle.Regular, GraphicsUnit.Point); // Substituted to FontA Font
+            Font numQ = new Font("Superspace Bold", (float)20, FontStyle.Regular, GraphicsUnit.Point);
+
+            e.Graphics.PageUnit = GraphicsUnit.Point;
+
+            // Draw the bitmap            
+            center = 50;
+            e.Graphics.DrawImage(DemoQue.Properties.Resources.cu2, 70, -8, 80, 95);
+
+            // Print the receipt text
+            lineOffset = printFont.GetHeight(e.Graphics) - (float)0.5;
+            x = 10;
+            y = 75 + lineOffset;
+            e.Graphics.DrawString("   Smart Queue System", printFont, Brushes.Black, center, y);
+            y += lineOffset;
+            e.Graphics.DrawString("    TEL : 02-311-6881", printFont, Brushes.Black, center, y);
+            y += lineOffset;
+            e.Graphics.DrawString("  " + DateTime.Now.ToString(), printFont, Brushes.Black, center, y);
+            y += lineOffset;
+            e.Graphics.DrawString("     จำนวนคิวที่รอ 0 คิว", printFont, Brushes.Black, center, y);
+            y += lineOffset;
+
+            e.Graphics.DrawString("___________________________________", printFont, Brushes.Black, x, y);
+            y += 120;
+            //e.Graphics.DrawImage(WindowsFormsApp1.Properties.Resources._1, 60, 150, 100, 100);
+            e.Graphics.DrawImage(qrcode.Draw("001", 100), 60, 150, 100, 100);
+            y += lineOffset;
+
+            e.Graphics.DrawString("      001", numQ, Brushes.Black, center, y);
+            y += lineOffset;
+            e.Graphics.DrawString("___________________________________", printFont, Brushes.Black, x, y);
+
+            printFont = new Font("Superspace Bold", 14, FontStyle.Regular, GraphicsUnit.Point);
+            lineOffset = printFont.GetHeight(e.Graphics) - 3;
+            y += lineOffset;
+            e.Graphics.DrawString("Computer Union CO.,Ltd", printFont, Brushes.Black, 35, y);
+        }
+    }
+}
